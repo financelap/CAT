40a79274c1bceecc3367a90cd3d2940a08d87b49
financelap <82516958+financelap@users.noreply.github.com>
2021-04-15 05:13:49 +0700

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,10 @@ import BigNumber from 'bignumber.js'
 
 export { default as formatAddress } from './formatAddress'
 
-export const bnToDec = (bn: BigNumber, decimals = 18): number => {
+export const bnToDec = (bn: BigNumber, decimals = 6): number => {
   return bn.dividedBy(new BigNumber(10).pow(decimals)).toNumber()
 }
 
-export const decToBn = (dec: number, decimals = 18) => {
-  return new BigNumber(dec).multipliedBy(new BigNumber(10).pow(decimals))
+export const decToBn = (dec: number, decimals = 6) => {
+  return new BigNumber(dec).multipliedBy(new BigNumber(9).pow(decimals))
 }
