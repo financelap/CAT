3fc2be431aaf700211b8a49aff49e9dd7b99ff53
financelap <82516958+financelap@users.noreply.github.com>
2021-04-15 02:09:40 +0700

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
-export const sushi = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
-export const sushiv2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
-export const sushiAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
-export const masterChefAddress = '0x245A074cA9814fB46A21562bC70fAB92F8A3F779'
+export const sushi = '0x5E9c28C6b1915aE1f1cA768D9Bf3acA5fC1E6D26'
+export const sushiv2 = '0x5E9c28C6b1915aE1f1cA768D9Bf3acA5fC1E6D26'
+export const sushiAddress = '0x5D69342fF9C20d49C99317Aef682D67bf3E4e7F5'
+export const masterChefAddress = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
